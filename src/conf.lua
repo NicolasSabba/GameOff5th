2d6efcf2b758e16b81e58c57e0b5a97050d463b3
Nicolas <nicolas.sabbatini.95@gmail.com>
2018-02-18 18:28:58 -0300

    Fixed title and game idendtity

--- src/conf.lua
+++ src/conf.lua
@@ -2,9 +2,9 @@
 function love.conf(t)
 
 	t.version = '0.10.2'
-	t.window.title = 'exqueleton'
+	t.window.title = 'Space Battle 101'
 
 	t.window.vsync = false
 
-	t.identity = 'exqueleton'
+	t.identity = 'SpaceBattle101'
 end
